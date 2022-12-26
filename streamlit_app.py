b8692354a0c846f89db5e3f2a451c8067daccf37
Nikolay Petrov <nikolay.petrov@infinitelambda.com>
2022-12-27 01:16:17 +0700

    add fruit var to Fruityvice Fruit Advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.text(fruityvice_response.json())
 
 # Normalise the fruityvice json data
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
