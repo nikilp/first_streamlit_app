9c605675800ebf25ce5ed7d277e99908b06c3512
Nikolay Petrov <nikolay.petrov@infinitelambda.com>
2022-12-27 14:10:25 +0700

    introduced fruitvice function

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+def get_fruitvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruitvice_normalized
+
+
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -32,9 +39,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    fruitvice_data = get_fruitvice_data(fruit_choice)
+    streamlit.dataframe(fruityvice_data)
+
 except URLError as e:
   streamlit.error()
 
